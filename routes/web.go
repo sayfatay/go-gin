0969407b5ab9a4b0c87ce1658ee2cffc3b973a29
ll <sayfa.nam@fullteam.tech>
2022-05-22 22:51:10 +0700

    ##first commit

--- routes/web.go
+++ routes/web.go
@@ -7,23 +7,6 @@ import (
 )
 
 // WebRouter web router
-// func WebRouter(serviceName string, router *gin.Engine) *gin.Engine {
-// 	r := router.Group(fmt.Sprintf("/%s", serviceName)) // do not change
-// 	{
-// 		userAPI := new(httpHandler.UserAPIHandler)
-// 		r.Any("/", func(c *gin.Context) {
-// 			c.JSON(200, map[string]interface{}{
-// 				"code":    200,
-// 				"message": "success",
-// 			})
-// 		})
-// 		r.POST("/user/get", userAPI.GetUser)
-// 		// r.GET("/cdn/get-file", cdnAPI.GetFile)
-// 		// r.POST("/cdn/validate-file-member", cdnAPI.ValidateFileImportFileMember)
-// 		// r.POST("/cdn/import-file-member", cdnAPI.ImportFileMember)
-// 	}
-// 	return router
-// }
 func SetupRouter(r *gin.Engine) *gin.Engine {
 	// r := gin.Default()
 
